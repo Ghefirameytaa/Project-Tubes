40dfd90f827201f052354e70d6b297c564741435
Sandi Prasetyo <sandiprasetyo12@gmail.com>
2025-12-18 16:21:30 +0700

    Pembayaran

--- Bhumi_Bambu/app/Models/Pembayaran.php
+++ Bhumi_Bambu/app/Models/Pembayaran.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Pemesanan;
+use Illuminate\Databae\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pembayaran extends Model
--- Bhumi_Bambu/routes/web.php
+++ Bhumi_Bambu/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\LandingPageController;
+use App\Models\Pembayaran;
 
 
 Route::get('/', [LandingPageController::class, 'index']);
+
 
 // Route::get('/', function () {
 //     return view('welcome');
